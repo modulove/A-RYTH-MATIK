5b8c4f21b413be32b7670d8dbed49096bb4a1dcc
modulove <98668901+modulove@users.noreply.github.com>
2024-04-30 15:07:15 +0200

    Update ARYTHMATIK_Euclid.ino
    test USD Panel option

--- Firmware/ARYTHMATIK_Euclid/ARYTHMATIK_Euclid.ino
+++ Firmware/ARYTHMATIK_Euclid/ARYTHMATIK_Euclid.ino
@@ -131,6 +131,11 @@ void setup() {
   // OLED setting
   delay(1000); // Screen needs a sec to initialize
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  #ifdef PANEL_USD
+  display.setRotation(2);  // 180 degree rotation for upside-down use
+  #else
+  display.setRotation(0);  // Normal orientation
+  #endif
   display.setTextSize(1);
   display.setTextColor(WHITE);
   OLED_display();
